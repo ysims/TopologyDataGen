86371e4b2dc317510b8d538599ff0aac8d4d1e59
Ysobel <ysobelsims@gmail.com>
2021-04-22 20:10:35 +1000

    Make many voxel grids

--- DatasetCreation/Voxels/voxels_test.py
+++ DatasetCreation/Voxels/voxels_test.py
@@ -3,19 +3,24 @@ import numpy as np
 from mpl_toolkits.mplot3d import Axes3D # <--- This is important for 3d plotting 
 from Voxels3d import Voxels3d
 import math
+import itertools
 
-voxels = Voxels3d([30, 30, 30])
-voxels.addObjects(3)
-voxels.checkIntersectFull()
+voxel_dataset = []
 
-output = voxels.getObjects()
+for i in range(100):
+    voxels = Voxels3d([30,30,30])
+    voxels.add_objects(3)
+    voxel_dataset.append(voxels.get_objects())
+    voxels.check_intersect_full()
 
-# set the colors of each object
-colors = np.empty(output.shape, dtype=object)
-colors[output] = 'blue'
+
+for i,j in itertools.product(voxel_dataset,repeat=2):
+    if i == j:
+        print("THE SAME!")
 
 # and plot everything
-ax = plt.figure().add_subplot(projection='3d')
-ax.voxels(output, edgecolor='k')
+for i in voxel_dataset:
+    ax = plt.figure().add_subplot(projection='3d')
+    ax.voxels(i, edgecolor='k')
 
 plt.show()
