69dab43ef29e4f597a2e5de85af47760e47cecf7
Ysobel <ysobelsims@gmail.com>
2021-04-26 07:49:54 +1000

    Structure to collect a lot of data.

--- DatasetCreation/Voxels/voxels_test.py
+++ DatasetCreation/Voxels/voxels_test.py
@@ -6,19 +6,23 @@ import math
 import itertools
 import random
 
-# voxel_dataset = []
+voxel_dataset = []
  
-# for i in range(100):
-#     for j in range(3,10):
-#         for k in range(2,10):
-#             voxels = Voxels3d(30)
-#             voxels.add_objects(j,k)
-#             voxel_dataset.append(voxels.get_objects())
+
+for j in range(3,8):
+    for k in range(2,8):
+        for i in range(50):
+            voxels = Voxels3d(30)
+            voxels.add_objects(j,k)
+            voxel_dataset.append(voxels.get_objects())
+            print("-> Data added: ", i, "/50")
+    print("----> End k: ", k, "/8")
+print("-------------> End j:", j, "/8")
 
 
-# for i,j in itertools.combinations(voxel_dataset,2):
-#     if not (i ^ j).any():
-#         print("THE SAME!")
+for i,j in itertools.combinations(voxel_dataset,2):
+    if not (i ^ j).any():
+        print("THE SAME!")
 
 # plotting
 # for i in voxel_dataset:
@@ -26,10 +30,10 @@ import random
 #     ax.voxels(i, edgecolor='k')
 # plt.show()
 
-voxels = Voxels3d(30)
-voxels.add_objects(5,2)
+# voxels = Voxels3d(30)
+# voxels.add_objects(5,2)
 
 
-ax = plt.figure().add_subplot(projection='3d')
-ax.voxels(voxels.get_objects(), edgecolor='k')
-plt.show()
+# ax = plt.figure().add_subplot(projection='3d')
+# ax.voxels(voxels.get_objects(), edgecolor='k')
+# plt.show()
