1fcd5812f44290f17df25500e5e108d4d7eba027
Ysobel <ysobelsims@gmail.com>
2021-07-29 13:53:59 +1000

    Refactor data generation scripts

---
+++ DatasetCreation/Voxels/generate.py
@@ -0,0 +1,105 @@
+import argparse
+import sys
+
+sys.path.append("scripts")
+
+from dataset import generate_dataset
+from single import create_single_data
+
+# ***** OPTIONS *****
+# Generic
+parser = argparse.ArgumentParser(
+    description="This program creates and draws cavity-filled cube/s"
+)
+parser.add_argument(
+    "first_type",
+    default="single",
+    choices=["single", "multi"],
+    help="Type of generation - a single cube or multiple.",
+)
+parser.add_argument(
+    "--cube_size", type=int, default=50, help="Size of the cavity-filled cube, cubed."
+)
+
+# Single specific
+parser.add_argument(
+    "--spheroid_num",
+    type=int,
+    default=0,
+    help="Number of spheroid cavities to add to the cube.",
+)
+parser.add_argument(
+    "--torus_num",
+    type=int,
+    default=0,
+    help="Number of torus cavities to add to the cube.",
+)
+parser.add_argument(
+    "--torusN_num",
+    type=int,
+    default=0,
+    help="Number of n-holed torus cavities to add to the cube.",
+)
+parser.add_argument(
+    "--island_num",
+    type=int,
+    default=0,
+    help="Number of island cavities to add to the cube.",
+)
+parser.add_argument(
+    "--tunnel_num",
+    type=int,
+    default=0,
+    help="Number of tunnel cavities to add to the cube.",
+)
+parser.add_argument(
+    "--octopus_num",
+    type=int,
+    default=0,
+    help="Number of octopus cavities to add to the cube.",
+)
+parser.add_argument(
+    "--draw", action="store_true", default=False, help="Draws the cube."
+)
+parser.add_argument(
+    "--save", action="store_true", default=False, help="Save the data with numpy."
+)
+
+# Dataset specific
+# TODO: Allow for multiple objects
+parser.add_argument(
+    "--object",
+    choices=["spheroid", "torus", "torusN", "island", "tunnel", "octopus"],
+    help="What object type to generate",
+)
+parser.add_argument(
+    "--min_objects",
+    type=int,
+    default=1,
+    help="The smallest number of objects in any one data point in the dataset",
+)
+parser.add_argument(
+    "--max_objects",
+    type=int,
+    default=5,
+    help="The highest number of objects in any one data point in the dataset",
+)
+parser.add_argument(
+    "--repeat",
+    type=int,
+    default=1000,
+    help="Number of times to repeat on one set of parameters.",
+)
+
+args = parser.parse_args()
+
+# **** MAKE THE DATA ****
+# Create just one cube with given parameters
+if args.first_type == "single":
+    create_single_data(args)
+
+# Create a dataset of cubes
+else:
+    if args.object is None:
+        parser.error("Dataset mode requires --object")
+    generate_dataset(args)
---
+++ DatasetCreation/Voxels/scripts/single.py
@@ -0,0 +1,86 @@
+import itertools
+import time
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D  # <--- This is important for 3d plotting
+import numpy as np
+import os
+import random
+import yaml
+
+from BettiCube import BettiCube
+
+# Create a single cube
+def create_single_data(args):
+    # Create the dictionary to print and use when making objects
+    dict = {
+        "Spheroid": args.spheroid_num,
+        "Torus": args.torus_num,
+        "TorusN": args.torusN_num,
+        "Island": args.island_num,
+        "Tunnel": args.tunnel_num,
+        "Octopus": args.octopus_num,
+    }
+
+    print("Adding: ", dict)
+    start_time = time.time()
+
+    # Create the cube with the given holes
+    voxels = BettiCube(args.cube_size)
+    voxels.add_objects(dict)
+
+    print("Adding --------- %s seconds ---" % (time.time() - start_time))
+
+    # Draw the grid with matplotlib
+    if args.draw:
+        ax = plt.figure().add_subplot(projection="3d")
+        ax.voxels(voxels.get_objects(draw=True), edgecolor="k")
+        plt.show()
+
+    # Save the cube with numpy
+    if args.save:
+        path = os.path.join(os.getcwd(), "all_data/single")
+
+        # Create the directory to store any data
+        try:
+            os.mkdir(path)
+        except:
+            pass
+
+        # Create the numpy array for the inverted cube
+        grid = voxels.get_objects(draw=True)
+        numpy_point_cloud = None
+        for X, Y, Z in itertools.product(range(0, args.cube_size), repeat=3):
+            if grid[X][Y][Z]:
+                if type(numpy_point_cloud) is np.ndarray:
+                    numpy_point_cloud = np.concatenate(
+                        (numpy_point_cloud, [[X, Y, Z]]), axis=0
+                    )
+                else:
+                    numpy_point_cloud = np.array([[X, Y, Z]])
+        # Save the inverted cube
+        r_num = random.randint(0, 99999)
+        np.save(
+            os.path.join(path, "{r_num}_inverted_cube".format(r_num=r_num)),
+            numpy_point_cloud,
+        )
+
+        # Make the normal cube
+        full_grid = voxels.get_full_objects()  # get the objects
+        numpy_point_cloud = None
+        for X, Y, Z in itertools.product(range(0, 30), repeat=3):
+            if full_grid[X][Y][Z]:
+                if type(numpy_point_cloud) is np.ndarray:
+                    numpy_point_cloud = np.concatenate(
+                        (numpy_point_cloud, [[X, Y, Z]]), axis=0
+                    )
+                else:
+                    numpy_point_cloud = np.array([[X, Y, Z]])
+        # Save the normal cube
+        np.save(
+            os.path.join(path, "{r_num}_cube".format(r_num=r_num)), numpy_point_cloud
+        )
+        # Save the metadata
+        with open(
+            os.path.join(path, "{r_num}_betti.yaml".format(r_num=r_num)), "w"
+        ) as file:
+            documents = yaml.dump(voxels.get_data(), file)
