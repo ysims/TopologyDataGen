22cb492fb9999b3838ce81b66cd3bb532706f07f
Ysobel <ysobelsims@gmail.com>
2021-09-06 15:45:13 +1000

    Split up ripser plotting

--- LibraryTesting/open-ripser.py
+++ LibraryTesting/open-ripser.py
@@ -1,8 +1,19 @@
+import argparse
 import pickle
 from persim import plot_diagrams
 
+parser = argparse.ArgumentParser(
+    description="This program loads Ripser output and plots it."
+)
+parser.add_argument(
+    "ripser_file",
+    help="The file path to the ripser output to load.",
+)
+
+args = parser.parse_args()
+
 # Open the pickled file
-with open("output.txt", "rb") as fp:  # Unpickling
+with open(args.ripser_file, "rb") as fp:  # Unpickling
     diagram = pickle.load(fp)
 
 # Plot the persistent homology
--- LibraryTesting/run-ripser.py
+++ LibraryTesting/run-ripser.py
@@ -1,21 +1,36 @@
+import argparse
+import numpy as np
+import pickle
 from ripser import ripser
 import time
-import pickle
-import numpy as np
 
-data = np.load("point_cloud.npy")
+parser = argparse.ArgumentParser(
+    description="This program loads a voxel grid and runs Ripser on the grid."
+)
+parser.add_argument(
+    "input_file",
+    help="The file path to the grid to load.",
+)
+parser.add_argument(
+    "output_file",
+    help="The file path to save the result to.",
+)
+
+args = parser.parse_args()
+
+data = np.load(args.input_file)
 
 # We want to time the computation - start the clock
 start = time.perf_counter()
 print("Begin!")
 
 # Calculate the persistent homology
-diagram = ripser(data, maxdim=3)["dgms"]
+diagram = ripser(data, maxdim=3, thresh=2)["dgms"]
 
 # Finish timing and print the time it took.
 end = time.perf_counter()
 print("Computation took ", (end - start), " seconds")
 
 # Save the diagram by picking
-with open("output.txt", "wb") as fp:  # Pickling
+with open(args.output_file, "wb") as fp:  # Pickling
     pickle.dump(diagram, fp)
