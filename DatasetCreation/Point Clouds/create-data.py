d0355f1671b552498aa1bdf8d2e3b7eeb0ab1d68
Ysobel <ysobelsims@gmail.com>
2021-09-06 15:41:16 +1000

    Formatting of point cloud gen + ripser

--- DatasetCreation/Point Clouds/create-data.py
+++ DatasetCreation/Point Clouds/create-data.py
@@ -8,7 +8,7 @@ from persim import plot_diagrams
 import random
 from PointCloud import PointCloud
 
-# # Returns the distance between coord1 and coord2, removing the third dimension. Both coordinates are 3d cartesian coordinates. 
+# # Returns the distance between coord1 and coord2, removing the third dimension. Both coordinates are 3d cartesian coordinates.
 # def dist2d(coord1, coord2):
 #     return math.sqrt(math.pow((coord1[1] - coord2[1]), 2) + math.pow((coord1[2] - coord2[2]), 2))
 
@@ -32,7 +32,7 @@ min_radius = 5
 radius_outer = 16
 radius_inner = 5
 point = np.array([15, 15, 15])
-direction = np.array([1,1,1])
+direction = np.array([1, 1, 1])
 height = 15
 
 # Create the cube as a numpy array
@@ -47,24 +47,24 @@ data = cube.get_data()
 #         f.write("%s\n" % item)
 
 # Plot the data points
-ax = plt.axes(projection='3d')
+ax = plt.axes(projection="3d")
 
 # plt.plot(data[:,0],data[:,1], 'o')
 
-slice = 17
-start = slice * (square_size * square_size)
-if start != 0:
-    start = start + 1
-end = (slice+1) * (square_size * square_size)
+# slice = 17
+# start = slice * (square_size * square_size)
+# if start != 0:
+#     start = start + 1
+# end = (slice+1) * (square_size * square_size)
 
 # ax.scatter3D(data[start:end,0], data[start:end,1], data[start:end,2])
-ax.scatter3D(data[:,0], data[:,1], data[:,2])
-plt.show()
+# ax.scatter3D(data[:, 0], data[:, 1], data[:, 2])
+# plt.show()
 
-# print('Computing topology')
+print("Computing topology")
 
 # Calculate the persistent homology
-# diagrams = ripser(data, maxdim=3)['dgms']
+diagrams = ripser(data, maxdim=3)["dgms"]
 
 # # Plot the persistent homology
-# plot_diagrams(diagrams, show=True)
+plot_diagrams(diagrams, show=True)
