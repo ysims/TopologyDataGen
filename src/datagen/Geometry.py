529ccab6980e7f765b8e1d64e6fff0252930bdba
Ysobel <ysobelsims@gmail.com>
2021-11-16 09:55:21 +1100

    Changes rotation function

--- src/datagen/Geometry.py
+++ src/datagen/Geometry.py
@@ -1,80 +1,41 @@
+import math
 import numpy as np
 import itertools
-import time
-
-# General function for creating a homogeneous transformation matrix
-# made up of X-Y-Z rotations
-# rotation is a 3d array of x rotation,
-# y rotation, z rotation applied that order.
-def get_rotation(rotation):
-    X = np.array(
-        [
-            [1.0, 0.0, 0.0, 0.0],
-            [0.0, np.cos(rotation[0]), np.sin(rotation[0]), 0.0],
-            [0.0, -np.sin(rotation[0]), np.cos(rotation[0]), 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-    Y = np.array(
-        [
-            [np.cos(rotation[1]), 0.0, -np.sin(rotation[1]), 0.0],
-            [0.0, 1.0, 0.0, 0.0],
-            [np.sin(rotation[1]), 0.0, np.cos(rotation[1]), 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-    Z = np.array(
-        [
-            [np.cos(rotation[2]), np.sin(rotation[2]), 0.0, 0.0],
-            [-np.sin(rotation[2]), np.cos(rotation[2]), 0.0, 0.0],
-            [0.0, 0.0, 1.0, 0.0],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-    return Z.dot(Y.dot(X))
-
-
-# R is a rotation matrix
-# C is the center about which we want to rotate - in most cases,
-#   the center of the shape we are rotating
-# P is a point x,y,z which we are rotating about the center
-#   by the angled defined in rotation
-def rotate(R, C, P):
-    # Homogeneous transformation matrices with the
-    # negative center and positive center
-    T_neg = np.array(
-        [
-            [1.0, 0.0, 0.0, -C[0]],
-            [0.0, 1.0, 0.0, -C[1]],
-            [0.0, 0.0, 1.0, -C[2]],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-    T_pos = np.array(
-        [
-            [1.0, 0.0, 0.0, C[0]],
-            [0.0, 1.0, 0.0, C[1]],
-            [0.0, 0.0, 1.0, C[2]],
-            [0.0, 0.0, 0.0, 1.0],
-        ]
-    )
-    # Turn the point into a form we can multiply with
-    P_full = [P[0], P[1], P[2], 1.0]
-    # Return C*R*(-C)*P - move the origin to the center,
-    # rotate the point, then move the origin back
-    return T_pos.dot(R.dot(T_neg.dot(P_full)))[:3]
-
+import scipy.ndimage
 
 # Returns a rotated grid of indices
 def rotate_grid(size, rotation, center):
-    x, y, z = np.indices((size, size, size))
-    R = get_rotation(rotation)
+    dimensions = len(center)
+    grid = np.indices([size * 2 for _ in range(dimensions)])
 
-    for X, Y, Z in itertools.product(range(0, size), repeat=3):
-        x[X][Y][Z], y[X][Y][Z], z[X][Y][Z] = rotate(R, center, [X, Y, Z])
+    grid[0,0:size*2, 0:size*2, 0:size*2] += center[0] - size
+    grid[1,0:size*2, 0:size*2, 0:size*2] += center[1] - size
+    grid[2,0:size*2, 0:size*2, 0:size*2] += center[2] - size
 
-    return x, y, z
+    # Scipy uses degrees, so convert rotation from radians to degrees
+    degrees_rotation = [x * 180 / math.pi for x in rotation]
+    # Get all possible axes to rotate on, and rotate on them
+    axes = [x for x in range(dimensions)]
+    for axis_1, axis_2 in itertools.combinations(axes, 2):
+        current_rotation = degrees_rotation[0]
+        degrees_rotation.pop(0)
+        for i in range(0, len(grid)):
+            grid[i] = scipy.ndimage.rotate(
+                grid[i],
+                current_rotation,
+                axes=(axis_1, axis_2),
+                reshape=False,
+            )
+    # Reverse the padding
+    start_slice = [0]
+    end_slice = [dimensions]
+    for i in range(dimensions):
+        start_slice.append(size - center[i])
+        end_slice.append((size * 2) - center[i])
 
+    grid = grid[tuple(map(slice, start_slice, end_slice))]
+
+    return grid
 
 # Get the distance in 3d between these two points
 def distance3d(point1, point2):
@@ -90,10 +51,12 @@ def distance3d(point1, point2):
 def intersect_or_touch(point, grid, object_min_distance):
     # Check if it hits the boundary
     if (max(point) > grid[0][0].size - 1) or min(point) < 0:
+        # print("true 1")
         return True
 
     # Check if this point intersects
     if grid[point[0]][point[1]][point[2]]:
+        # print("true 2")
         return True
 
     # Iteratively check the area around each position which was checked
@@ -115,6 +78,7 @@ def intersect_or_touch(point, grid, object_min_distance):
                     if grid[recurse_point[0] + x][recurse_point[1] + y][
                         recurse_point[2] + z
                     ]:
+                        # print("true 3")
                         return True
                 except:
                     continue
@@ -137,32 +101,27 @@ def obj_intersect_touch(point, grid, object_min_distance):
         # So we don't want it
         return True
 
-    # Iteratively check the area around each position which was checked
-    # with object_min_distance iterations.
-    recurse_points = [point]
-    new_recurse_points = []
-    for _ in range(object_min_distance):
-        for recurse_point in recurse_points:
-            # Check if the point touches anything in the grid,
-            # but ignore if it's on the border
-            for x, y, z in itertools.product([-1, 0, 1], repeat=3):
-                try:  # skip if this is out of bounds
-                    check_point = [
-                        recurse_point[0] + x,
-                        recurse_point[1] + y,
-                        recurse_point[2] + z,
-                    ]
-                    if new_recurse_points.count(check_point) == 0:
-                        new_recurse_points.append(check_point)
-                    if grid[check_point[0]][check_point[1]][check_point[2]]:
-                        if (max(check_point) < grid[0][0].size - 1) and min(
-                            check_point
-                        ) > 0:
-                            return True
-                except:
-                    continue
-        recurse_points = new_recurse_points
-        new_recurse_points = []
+    # Check if the point touches anything in the grid,
+    # but ignore if it's on the border
+    prod = [0]
+    for i in range(object_min_distance):
+        prod.append(i)
+        prod.append(-i)
+    for x, y, z in itertools.product(prod, repeat=3):
+        try:  # skip if this is out of bounds
+            check_point = [
+                point[0] + x,
+                point[1] + y,
+                point[2] + z,
+            ]
+            if grid[check_point[0]][check_point[1]][check_point[2]]:
+                if (max(check_point) < grid[0][0].size - 1) and min(
+                    check_point
+                ) > 0:
+                    return True
+        except:
+            continue
+
     # Nothing is wrong, so return false
     return False
 
@@ -198,27 +157,3 @@ def hard_surrounded(point, grid):
             continue
     # We got through without an empty point - we are surrounded!
     return True
-
-
-# Rotate just the object, not the whole grid
-def rotate_object(object, grid):
-    R = get_rotation(object.rotation)  # get the rotation matrix
-
-    # Loop for all indices but only do something
-    # if it's a voxel in the object
-    for X, Y, Z in itertools.product(range(0, grid[0][0].size), repeat=3):
-        if grid[X][Y][Z]:
-            x, y, z = rotate(R, object.center, [X, Y, Z])
-            try:
-                # Make the new position true (moving the voxel to here)
-                grid[int(round(x))][int(round(y))][int(round(z))] = True
-            except:
-                # The point could rotate out of the grid.
-                # This'll be flagged as intersecting with the
-                # boundary anyway, but we could handle this in the
-                # future by returning a bool or something
-                # that might quicken things up by stopping early
-                # if it's obviously not gonna work
-                pass
-            # The old position is now false (voxel has gone)
-            grid[X][Y][Z] = False
