5eb24a1b97ebc259447f0a263e455be575b13cb1
Ysobel <ysobelsims@gmail.com>
2021-11-18 16:21:57 +1100

    Update method input/output

--- src/scripts/data/augmentation/make_shell.py
+++ src/scripts/data/augmentation/make_shell.py
@@ -3,14 +3,12 @@ import torch
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 
-# Creating a box with a sphere in it
-def make_shell(grid):
-    box = np.zeros((62, 62, 62))
-    size = grid[0][0].size
-    box[3:size+3, 3:size+3, 3:size+3] = grid
+# Convert a grid into a shell
+def make_shell(input_file, output_file):
+    grid = np.load(input_file)
 
     # Convert the box to a torch tensor
-    box_t = torch.from_numpy(box).float().unsqueeze(0).unsqueeze(0)
+    box_t = torch.from_numpy(grid).float().unsqueeze(0).unsqueeze(0)
 
     # Create the 3D kernel
     conv = torch.nn.Conv3d(1, 1, 3, stride=1, bias=False, padding=1)
@@ -32,10 +30,11 @@ def make_shell(grid):
     # Overwrite the convolution kernel weight with the custom kernel
     conv.weight = torch.nn.Parameter(custom_conv.unsqueeze(0).unsqueeze(0))
     convolved = conv(box_t)     # Convolve the box
-
-    # Need to collapse the additional dimensions from the 3D convolution (batch and channel dimensions)
-    convolved_clone = convolved.clone().detach().squeeze().squeeze()
-    indices_t = torch.from_numpy(np.indices(convolved_clone.shape))
+    convolved = torch.clamp(convolved, min=0, max=1) 
     
+    # Need to collapse the additional dimensions from the 3D convolution (batch and channel dimensions)
+    convolved = convolved.detach().squeeze().squeeze()
 
-    print(np.array(indices_t))
+    np.save(output_file, convolved.numpy())
+
+    
